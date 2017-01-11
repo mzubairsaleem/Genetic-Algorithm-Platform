2f3212f012f14d66dbae1bf4e60765f4ef47bfe4
Oren (electricessence) <oren@electrified.net>
2017-01-10 16:56:58 -0800

    Improved API

--- source/IGenomeFactory.cs
+++ source/IGenomeFactory.cs
@@ -11,23 +11,29 @@ namespace GeneticAlgorithmPlatform
 	public interface IGenomeFactory<TGenome>
 	 where TGenome : class, IGenome
 	{
+		/*
+		 * Note that the input collections are all arrays for some important reasons:
+		 * 1) The underlying computation does not want an enumerable that can change in size while selecing for a genome.
+		 * 2) In order to select at random, the length of the collection must be known.
+		 * 3) Forces the person using this class to smartly think about how to provide the array.
+		 */
 
 		TGenome GenerateOne(TGenome source);
 
-		TGenome GenerateOne(IEnumerable<TGenome> source = null);
+		TGenome GenerateOne(TGenome[] source = null);
 
 		IEnumerable<TGenome> Generate(TGenome source);
 
-		IEnumerable<TGenome> Generate(IEnumerable<TGenome> source = null);
+		IEnumerable<TGenome> Generate(TGenome[] source = null);
 
 		bool AttemptNewMutation(TGenome source, out TGenome mutation, byte triesPerMutationLevel = 5, byte maxMutations = 3);
 
-		bool AttemptNewMutation(IEnumerable<TGenome> source, out TGenome mutation, byte triesPerMutationLevel = 5, byte maxMutations = 3);
+		bool AttemptNewMutation(TGenome[] source, out TGenome mutation, byte triesPerMutationLevel = 5, byte maxMutations = 3);
 
 		IEnumerable<TGenome> Mutate(TGenome source);
 
 		bool AttemptNewCrossover(TGenome a, TGenome b, out TGenome[] offspring, byte maxAttempts = 3);
 		
-		bool AttemptNewCrossover(IEnumerable<TGenome> source, out TGenome[] offspring, byte maxAttemptsPerCombination = 3);
+		bool AttemptNewCrossover(TGenome[] source, out TGenome[] offspring, byte maxAttemptsPerCombination = 3);
 	}
 }
